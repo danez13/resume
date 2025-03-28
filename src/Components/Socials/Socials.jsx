87bbafbc25922ad8c88072aa4e06c5855f828ef8
danez13 <hernandezdaniel1702@gmail.com>
2025-03-28 16:26:38 -0400

    Updates

--- src/Components/Socials/Socials.jsx
+++
@@ -1,8 +0,0 @@
-function Socials({link="https://www.youtube.com/watch?v=dQw4w9WgXcQ",label="PlaceHolder",image="https://placehold.co/600x400",alt="PlaceHolder"}){
-    return (
-        <a href={link} aria-label={label}>
-            <img src={image} alt={alt}></img>
-        </a>
-    );
-}
-export default Socials
